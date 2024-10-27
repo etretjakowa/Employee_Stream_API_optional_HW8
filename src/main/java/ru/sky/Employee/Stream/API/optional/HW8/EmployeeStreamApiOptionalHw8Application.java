8ae64d88008fa90fe978d9edd3334002ade1aac3
etretjakova <etretjakowa@yandex.ru>
2024-10-27 23:00:12 +0300

    initial commit

---
+++ src/main/java/ru/sky/Employee/Stream/API/optional/HW8/EmployeeStreamApiOptionalHw8Application.java
@@ -0,0 +1,13 @@
+package ru.sky.Employee.Stream.API.optional.HW8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeStreamApiOptionalHw8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeStreamApiOptionalHw8Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sky/Employee/Stream/API/optional/HW8/EmployeeStreamApiOptionalHw8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sky.Employee.Stream.API.optional.HW8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeStreamApiOptionalHw8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
